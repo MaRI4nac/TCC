46ab41a051fb73c6c29f491fb4a0d171fd847cd9
Mariana <ra36816458842@acaonsfatima.org.br>
2021-10-16 16:00:41 -0300

    + estrutura pag. inicial (organizacao)

--- reactjs/src/index-principal.js
+++ reactjs/src/index-principal.js
@@ -6,19 +6,9 @@ import { Botao } from "./components/botoes/styled";
 export default function SiteCompleto () {
     return (
         <div className="organizacao-principal">
-            <Botao className="idai"> <Link to="/criarconta"> Criar Conta </Link> </Botao>
-            <Botao className="idai"> <Link to="/cabecalho"> Cabeçalho </Link> </Botao>
             <Botao className="idai"> <Link to="/logar"> Login (Usuário) </Link> </Botao>
-            <Botao className="idai"> <Link to="/compra"> Confirmar Compra </Link> </Botao>
-            <Botao className="idai"> <Link to="/esqueceusenha"> Esqueceu Senha </Link> </Botao>
-            <Botao className="idai"> <Link to="/novasenha"> Nova Senha </Link> </Botao>
             <Botao className="idai"> <Link to="/admlogin"> ADM Login </Link> </Botao>
             <Botao className="idai"> <Link to="/buscadireta"> Busca Direta </Link> </Botao>
-            <Botao className="idai"> <Link to="/buscadirecionada"> Busca Directionada </Link> </Botao>
-            <Botao className="idai"> <Link to="/ingressos"> Ingressos </Link> </Botao>
-            <Botao className="idai"> <Link to ="/inicial"> Tela Inicial </Link></Botao>
-            <Botao className="idai"> <Link to ="/eventos"> Tela Evento </Link></Botao>
-            <Botao className="idai"> <Link to ="/profile"> Tela Perfil Usuário </Link></Botao>
         </div>
     )
 }
