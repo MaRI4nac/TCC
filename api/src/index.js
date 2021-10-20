cf6409e64b979dc6f9b19f2aac83834596a4639d
Mariana <ra36816458842@acaonsfatima.org.br>
2021-10-20 16:48:22 -0300

    ok

--- api/src/index.js
+++ api/src/index.js
@@ -5,36 +5,8 @@ import cors from 'cors'
 const app = express();
 app.use(cors()); 
 
-app.get('/buscadireta/:nmEvento', async (req, resp) => {
-    try {
-        let r = await api.tb_evento.findAll( {where: { nm_evento: req.params.nmEvento}});
-        
-        resp.send(r);
-    }
-    catch {
-        resp.send( {erro: ""})
-    }
-})
 
-app.get('/buscadirecionada/:categoria', async (req, resp) => {
-    try {
-        let r = await api.tb_evento.findAll( {where: { ds_categoria: req.params.categoria }});
-        
-        resp.send(r);
-    }
-    catch {
-        resp.send( {erro: ""})
-    }
-})
 
-app.get('/eventosdestaque', async (req, resp) => {
-    try {
-       
-    }
-    catch {
-        resp.send( {erro: ""})
-    }
-})
 
 app.listen(process.env.PORT,
               x => console.log(`Server up at port ${process.env.PORT}`))
