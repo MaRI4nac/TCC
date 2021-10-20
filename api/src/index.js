f93e46ca79a1410513871f4193087d74fd341959
LevyCS <levyfronto@gmail.com>
2021-10-20 16:30:06 -0300

    ok

--- api/src/index.js
+++ api/src/index.js
@@ -43,7 +43,19 @@ app.post('/user/create', async(req, resp) => {
     try {
         let json = req.body;
         let parts = json.nascimento.split('-');
+        
+        let validacaoCpf = await db.infoc_nws_tb_usuario.findOne({where: {ds_cpf: json.cpf}})
+        if (validacaoCpf != null)
+            return resp.send( {erro: "Cpf já cadastrado"})
 
+        let validacaoEmail = await db.infoc_nws_tb_usuario.findOne({where: {ds_email: json.email}})
+        if (validacaoEmail != null) 
+            return resp.send( { erro: "Email já cadastrado"})
+        
+        let validacaoUsername = await db.infoc_nws_tb_usuario.findOne({where: {ds_username: json.username}})
+        if (validacaoUsername != null)
+            return resp.send({ erro: "Username já cadastrado"})
+        
         let r = await db.infoc_nws_tb_usuario.create({
             nm_usuario: json.nmUsu,
             ds_cpf: json.cpf,
@@ -66,9 +78,9 @@ app.get('/user/login/', async(req, resp) => {
         let confirm = await db.infoc_nws_tb_usuario.findOne({where: {ds_email: req.query.email, ds_senha: req.query.senha} })
         if (confirm == null) 
             return resp.send( {erro: "usuário não cadastrado"})
-            
+
         let r = await db.infoc_nws_tb_usuario.findOne( {where: { id_usuario: confirm.id_usuario }} );
-        resp.send(confirm);
+        resp.send(r);
     }
     catch (e) { 
         resp.send({erro: e.toString()})
