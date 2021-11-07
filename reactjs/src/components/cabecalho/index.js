616a0f003010b83ac78a1d4d668cfce6dacf79a3
LevyCS <levyfronto@gmail.com>
2021-11-07 12:00:39 -0300

    ok

--- reactjs/src/components/cabecalho/index.js
+++ reactjs/src/components/cabecalho/index.js
@@ -4,13 +4,31 @@ import { useHistory } from 'react-router'
 import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 
+function lerUsuarioLogado (navigation) {
+    let logado = Cookies.get('usuario-logado')
+    console.log(logado)
+    if (!logado) {
+        return false;
+    }
+    let usuarioLogado = JSON.parse(logado);
+    return usuarioLogado; 
+}
 
 export default function Cabecalho(props) {
+    
+    const navig = useHistory();
+    const usuarioLogado = lerUsuarioLogado(navig);
+    
+    const [user, setUser] = useState(usuarioLogado);
+
 
     const [search, setSearch] = useState();
-    const navig = useHistory();
-    const [user, setUser] = useState(JSON.parse(Cookies.get('usuario-logado')));
 
+    useEffect(() => {
+        console.log(user)
+    }, [user])
+   
+   
     function keyPress(event) {
 
         if(event.charCode === 13) {
@@ -19,13 +37,14 @@ export default function Cabecalho(props) {
     }
 
     return (
+
         <Container>
             <Link to="/inicial"> <img className="logo-image" src="/assets/images/LOGO1 TCC.png" alt="" /> </Link>
-            <input type="text" placeholder="Buscar eventos..." value={search} onChange={ e => setSearch(e.target.value) } onKeyPress={keyPress} />
-            {/* {!Cookies.get('usuario-logado') 
-                ? <Link to="/logar" className="no-decoration"> <div> Entrar </div> </Link> 
+            <input type="text" placeholder="Buscar eventos..." value={search} onChange={(e) => setSearch(e.target.value) } onKeyPress={keyPress} />
+            {user == false
+                ? <Link to="/logar" className="no-decoration"> <div> Entrar </div> </Link>
                 : <Link to="/profile"> <img className="perfil-image" src={user.img_perfil} alt="" /> </Link>
-            }  */}
+            } 
         </Container>
     )
 }
