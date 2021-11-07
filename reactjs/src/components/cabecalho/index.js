1109914964d580b12fe1cf5bc6b3de1364596ab6
Mariana <ra36816458842@acaonsfatima.org.br>
2021-11-07 11:41:33 -0300

    ok

--- reactjs/src/components/cabecalho/index.js
+++ reactjs/src/components/cabecalho/index.js
@@ -9,11 +9,9 @@ export default function Cabecalho(props) {
 
     const [search, setSearch] = useState();
     const navig = useHistory();
-    const [user, setUser] = useState();
+    // const [user, setUser] = useState(JSON.parse(Cookies.get('usuario-logado')));
 
-    useEffect(() => {
-        setUser(JSON.parse(Cookies.get('usuario-logado')))
-    }, [])
+
 
     function keyPress(event) {
 
@@ -24,12 +22,12 @@ export default function Cabecalho(props) {
 
     return (
         <Container>
-            <Link to="/inicial"> <img className="logo-image" src="/assets/images/LOGO1 TCC.png" alt="" /> </Link>
+            <Link to="/inicial"> <img className="logo-image" src="/assets/images/LOGO1 TCC.png" alt="" onClick={ () => Cookies.remove('usuario-logado')} /> </Link>
             <input type="text" placeholder="Buscar eventos..." value={search} onChange={ e => setSearch(e.target.value) } onKeyPress={keyPress} />
-            {!Cookies.get('usuario-logado') 
+            {/* {!Cookies.get('usuario-logado') 
                 ? <Link to="/logar" className="no-decoration"> <div> Entrar </div> </Link> 
                 : <Link to="/profile"> <img className="perfil-image" src={user.img_perfil} alt="" /> </Link>
-            } 
+            }  */}
         </Container>
     )
 }
