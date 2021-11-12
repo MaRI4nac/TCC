69ad8ff93a7c1d381fe5ccc7e54f1b9e089a484d
LevyCS <levyfronto@gmail.com>
2021-11-12 17:17:46 -0300

    ok

--- reactjs/src/service/apiBuy.js
+++ reactjs/src/service/apiBuy.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create ({
-    baseURL: 'http://localhost:3030/buy'
+    baseURL: 'https://tccinsf-newside.herokuapp.com/buy'
 })
 
 export default class Api {
