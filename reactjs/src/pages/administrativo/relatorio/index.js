a23e8a9a84de95810a1fa2d765b035f7848019b0
Mariana <ra36816458842@acaonsfatima.org.br>
2021-11-13 12:09:52 -0300

    ok

--- reactjs/src/pages/administrativo/relatorio/index.js
+++ reactjs/src/pages/administrativo/relatorio/index.js
@@ -1,8 +1,61 @@
 import { Relator } from "./styled"
 import { Botao } from "../../../components/botoes/styled"
+import { useState } from "react";
+import { Pie } from 'react-chartjs-2'
+import Api from "../../../service/apiAdmGeneral";
+const api = new Api();
 
 
 export default function ADMRelatorios () {
+    const[report, setReport] = useState([]);
+
+    async function listar(type) {
+        let e = await api.allReports(type);
+        setReport(e);
+    }
+
+    const pieGraphic = () => {
+        const data = {
+          labels: report.map(item => item.categoria), 
+          datasets: [
+            {
+              data: report.map(item => item.qtd),
+              backgroundColor: [
+                'rgba(21, 49, 49, 1)',
+                'rgba(36, 174, 174, 1)',
+                'rgba(41, 109, 109, 1)'
+              ],
+              borderColor: [
+                'rgba(41, 109, 109, 1)'
+
+              ],
+              borderWidth: 1,
+              hoverOffset: 4,
+              
+            },
+          ],
+        };
+        const options = {
+          layout: {
+            padding: 20
+        },
+          plugins: {
+            legend: {
+              labels: {
+                color: "black", 
+                font:{
+                  size: 15
+                }
+              }
+            },
+            
+          },
+        }
+        return  <Pie data={data} options={options} />
+      }
+
+
+
     return (
         <Relator>
         <div class="title"> Relatórios </div>
@@ -14,10 +67,10 @@ export default function ADMRelatorios () {
                         <div class="the-subtitle"> Gerado no dia 00/00/0000, às 23h59 </div>
                     </div>
                     <div class="the-graphic">
-                        <img src="/assets/images/graphics.svg" alt="" />
+                        <div> {pieGraphic()} </div>
                     </div>
                     <div class="the-button">
-                        <Botao> Gerar Relatório </Botao>
+                        <Botao onClick={() => listar('semanal')} > Gerar Relatório </Botao>
                     </div>
                 </div>
                 <div class="the-box">
@@ -26,10 +79,10 @@ export default function ADMRelatorios () {
                         <div class="the-subtitle"> Gerado no dia 00/00/0000, às 23h59 </div>
                     </div>
                     <div class="the-graphic">
-                        <img src="/assets/images/graphics.svg" alt="" />
+                        <div> {pieGraphic()} </div>
                     </div>
                     <div class="the-button">
-                        <Botao> Gerar Relatório </Botao>
+                        <Botao onClick={() => listar('mensal')}> Gerar Relatório </Botao>
                     </div>
                 </div>
             </div>
@@ -40,10 +93,10 @@ export default function ADMRelatorios () {
                         <div class="the-subtitle"> Gerado no dia 00/00/0000, às 23h59 </div>
                     </div>
                     <div class="the-graphic">
-                        <img src="/assets/images/graphics.svg" alt="" />
+                    <div> {pieGraphic()} </div>
                     </div>
                     <div class="the-button">
-                        <Botao> Gerar Relatório </Botao>
+                        <Botao onClick={() => listar('semestral')}> Gerar Relatório </Botao>
                     </div>
                 </div>
                 <div class="the-box">
@@ -52,10 +105,10 @@ export default function ADMRelatorios () {
                         <div class="the-subtitle"> Gerado no dia 00/00/0000, às 23h59 </div>
                     </div>
                     <div class="the-graphic">
-                        <img src="/assets/images/graphics.svg" alt="" />
+                        <div> {pieGraphic()} </div>
                     </div>
                     <div class="the-button">
-                        <Botao> Gerar Relatório </Botao>
+                        <Botao onClick={() => listar('anual')}> Gerar Relatório </Botao>
                     </div>
                 </div>
             </div>
